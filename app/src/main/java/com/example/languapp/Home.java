5f94e9d2cdb87828e14ba43cbf4918303f1bea66
Krasav4ik <zkonst12@gmail.com>
2020-04-30 23:00:46 +0300

    Made nav bar

--- app/src/main/java/com/example/languapp/Home.java
+++ app/src/main/java/com/example/languapp/Home.java
@@ -4,18 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.languapp.ui.home.HomeFragment;
-
 public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.container, HomeFragment.newInstance())
-                    .commitNow();
-        }
+        setContentView(R.layout.activity_home);
     }
 }
